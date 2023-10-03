f212c60893f6764ee48159959f79c718b890a5c4
@dmathcore231 <dmathcore231@gmail.com>
2023-10-03 13:06:14 +0300

    feat(FilterPanel): added multiSelect

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,7 +8,7 @@ export function Modal({ isActive, modalClass, title, onClose, onSubmit, children
   } else {
     return (
       <div className={`modal ${modalClass ? modalClass : ""}`}>
-        <div className="modal__content">
+        <div className="modal__content modal__content_height_100">
           <div className="modal__header">
             <h2 className="modal__title">{title}</h2>
             <button className="btn btn_close modal__btn-close" onClick={onClose}>
